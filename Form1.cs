360b72b667b353ae8a7d0b69fa5bbe13a2b338fd
bohemian9485 <tubianosaric26@gmail.com>
2022-07-19 14:52:46 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace TripleDesSolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    if (textBox2.Text != "")
+                    {
+                        MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                        UTF8Encoding utf8 = new UTF8Encoding();
+                        TripleDESCryptoServiceProvider tDES = new TripleDESCryptoServiceProvider();
+                        tDES.Key = md5.ComputeHash(utf8.GetBytes(textBox1.Text));
+                        tDES.Mode = CipherMode.ECB;
+                        tDES.Padding = PaddingMode.PKCS7;
+                        ICryptoTransform trans = tDES.CreateEncryptor();
+                        textBox3.Text = BitConverter.ToString(trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length));
+                        textBox4.Text = textBox3.TextLength.ToString();
+                    }
+                    else
+                    {
+                        textBox2.Focus();
+                        MessageBox.Show("Plain text not specified.", "Encryption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    textBox1.Focus();
+                    MessageBox.Show("Key not specified.", "Encryption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
